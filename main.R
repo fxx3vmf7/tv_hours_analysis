2cfbe17333346ab4333be224edddfc8b8377a39d
fxx3vmf7 <youlindi@gmail.com>
2026-02-07 18:46:31 -0500

    Filter to only those 30 years or younger

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ if (!require('pacman'))install.package('pacman')
 pacman::p_load(tidyverse,here)
 
 tv_hours_table<-gss_cat |> 
-  filter(age > 30) |> 
+  filter(age <= 30) |> 
   group_by(marital) |> 
   summarise(mean_tv_hours=mean(tvhours,na.rm=TRUE))
 write_csv(tv_hours_table,here('TV_Hours_by_Marital.csv'))
