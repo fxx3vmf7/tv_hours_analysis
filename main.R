5d11a649edc8226fccd05b8016732a181714b362
fxx3vmf7 <youlindi@gmail.com>
2026-02-07 15:56:30 -0500

    filtered to those aged 30 years or above

---
+++ main.R
@@ -0,0 +1,8 @@
+if (!require('pacman'))install.package('pacman')
+pacman::p_load(tidyverse,here)
+
+tv_hours_table<-gss_cat |> 
+  filter(age > 30) |> 
+  group_by(marital) |> 
+  summarise(mean_tv_hours=mean(tvhours,na.rm=TRUE))
+write_csv(tv_hours_table,here('TV_Hours_by_Marital.csv'))
